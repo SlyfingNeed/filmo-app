3d64bcd8d86826fa97afbb23e18427a7b81ce88e
SlyfingNeed <51042989+SlyfingNeed@users.noreply.github.com>
2024-05-28 17:18:57 +0700

    add navbar

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const NAV_LINKS = [
+    {href: '/', key: 'home', label: 'Home'},
+    {href: '/', key: 'history', label: 'History'},
+    {href: '/', key: 'how_filmo_work', label: 'How It Works?'},
+];
